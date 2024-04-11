3ee01f823209f12080f31ef387416a7406cb7100
Jaime Salazar <133693889+Titian06@users.noreply.github.com>
2024-04-10 19:07:36 -0600

    Add files via upload

---
+++ testpersonality/testpersonality/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testpersonality
+//
+//  Created by Alumno on 10/04/24.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
